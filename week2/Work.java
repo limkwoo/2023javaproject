220dd77cd22878f391bcb85749d33c9c97e9bb25
limkwoo <128570797+limkwoo@users.noreply.github.com>
2023-03-22 18:06:07 +0900

    Add files via upload

---
+++ week2/Work.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+
+
+public class Work {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		float x = sc.nextFloat();
+		
+		float y = sc.nextFloat();
+		
+		float z = sc.nextFloat();
+		
+		System.out.println(x+y+z);
+		
+	}
+
+}
