32aa3b925a3584e7a0db9934847c8f9d89b2b7c5
limkwoo <128570797+limkwoo@users.noreply.github.com>
2023-03-22 14:26:22 +0900

    Add files via upload

---
+++ week2/Hello.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java World!!");
+		System.out.println();
+	}
+
+}
